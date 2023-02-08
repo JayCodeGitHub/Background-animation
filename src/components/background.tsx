cfacb445d7ee5563ebc628fe62c93c6c29d4af43
JayCodeGitHub <JayCode.contakt@gmail.com>
2023-02-08 16:16:12 +0100

    Fix background height problem

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -4,21 +4,16 @@ export default function Background() {
   let { scrollYProgress } = useScroll();
   let y = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
   return (
-    <motion.div
-      style={{ y }}
-      className="absolute top-0 left-0 w-full h-full bg-gray-900 -z-20"
-    >
+    <motion.div className="absolute top-0 left-0 w-full h-full bg-gray-900 -z-20">
       <video
         muted
         playsInline
         autoPlay
         loop
-        className="top-0 left-0 object-cover object-center w-full h-[99%] opacity-50 -z-10"
+        className="top-0 left-0 object-cover object-center w-full h-screen opacity-80 -z-10"
         src="https://www.w3schools.com/howto/rain.mp4"
       ></video>
-      <div className="absolute top-0 left-0 w-full h-full -inset-px bg-gradient-to-t from-gray-900 to-transparent" />
-      <div className="h-full bg-gray-900" />
-      <div className="h-full bg-gray-900" />
+      <div className="absolute top-0 left-0 w-full h-screen -inset-px bg-gradient-to-t from-gray-900 to-transparent" />
     </motion.div>
   );
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,7 +10,7 @@ function Layout({ children }: LayoutProps) {
     <>
       <Background />
       <NavBar />
-      <main>{children}</main>
+      {children}
     </>
   );
 }
