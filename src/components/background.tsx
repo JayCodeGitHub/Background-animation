c27164dfa782e89160e1e577518bb7cf887ec21b
JayCodeGitHub <JayCode.contakt@gmail.com>
2023-02-08 17:02:03 +0100

    Added Animated Background

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,19 +1,10 @@
-import { motion, useScroll, useTransform } from "framer-motion";
+import Particles from "./particles";
 
 export default function Background() {
-  let { scrollYProgress } = useScroll();
-  let y = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
   return (
-    <motion.div className="absolute top-0 left-0 w-full h-full bg-gray-900 -z-20">
-      <video
-        muted
-        playsInline
-        autoPlay
-        loop
-        className="top-0 left-0 object-cover object-center w-full h-screen opacity-80 -z-10"
-        src="https://www.w3schools.com/howto/rain.mp4"
-      ></video>
+    <div className="absolute top-0 left-0 w-full h-full bg-gray-900 -z-10">
+      <Particles />
       <div className="absolute top-0 left-0 w-full h-screen -inset-px bg-gradient-to-t from-gray-900 to-transparent" />
-    </motion.div>
+    </div>
   );
 }
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,7 +4,70 @@ export default function About() {
       <header className="flex items-center w-full pl-8 text-5xl text-white h-96">
         <h1>About</h1>
       </header>
-      <section className="text-xl text-white px-36">
+      <section className="pb-10 text-xl text-white px-36">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Potenti
+          nullam ac tortor vitae purus faucibus ornare suspendisse. Dolor sit
+          amet consectetur adipiscing elit duis tristique sollicitudin nibh.
+          Pharetra diam sit amet nisl. Imperdiet proin fermentum leo vel orci.
+          Blandit cursus risus at ultrices mi tempus imperdiet nulla malesuada.
+          Ipsum dolor sit amet consectetur adipiscing. Sem viverra aliquet eget
+          sit amet tellus cras. Arcu non sodales neque sodales ut. Sed libero
+          enim sed faucibus turpis in eu. Suspendisse faucibus interdum posuere
+          lorem ipsum dolor. Ornare aenean euismod elementum nisi quis eleifend.
+          Tortor id aliquet lectus proin nibh. Amet cursus sit amet dictum sit
+          amet justo. Nunc faucibus a pellentesque sit. Vel eros donec ac odio
+          tempor orci dapibus. Volutpat ac tincidunt vitae semper quis lectus
+          nulla. Egestas sed sed risus pretium quam vulputate. Sagittis nisl
+          rhoncus mattis rhoncus. Interdum consectetur libero id faucibus nisl.
+          Pellentesque id nibh tortor id. Commodo ullamcorper a lacus vestibulum
+          sed arcu. Aliquam purus sit amet luctus venenatis lectus. Fringilla
+          phasellus faucibus scelerisque eleifend donec pretium vulputate.
+          Pulvinar elementum integer enim neque volutpat ac. Nunc sed augue
+          lacus viverra vitae congue. Velit laoreet id donec ultrices tincidunt
+          arcu. Malesuada fames ac turpis egestas maecenas pharetra convallis
+          posuere. Diam maecenas sed enim ut sem viverra aliquet eget. In
+          iaculis nunc sed augue lacus viverra. Feugiat nibh sed pulvinar proin.
+          Purus sit amet luctus venenatis lectus magna fringilla. Lectus magna
+          fringilla urna porttitor rhoncus dolor purus non. Etiam sit amet nisl
+          purus in. Nisl purus in mollis nunc sed id semper. Eget aliquet nibh
+          praesent tristique magna sit amet purus gravida. Vulputate odio ut
+          enim blandit volutpat maecenas volutpat. Leo vel orci porta non
+          pulvinar neque. Nibh mauris cursus mattis molestie a. Amet nisl purus
+          in mollis nunc sed id semper. Nisl nunc mi ipsum faucibus vitae. Est
+          pellentesque elit ullamcorper dignissim cras tincidunt. Sed felis eget
+          velit aliquet sagittis id consectetur purus ut. Arcu odio ut sem nulla
+          pharetra diam. Viverra nam libero justo laoreet sit amet cursus sit
+          amet. Cras ornare arcu dui vivamus arcu. Venenatis urna cursus eget
+          nunc scelerisque viverra. Nibh tortor id aliquet lectus proin nibh
+          nisl. Volutpat est velit egestas dui. Sit amet cursus sit amet dictum
+          sit amet justo. Ullamcorper morbi tincidunt ornare massa eget. Aliquam
+          id diam maecenas ultricies mi eget mauris pharetra et. Lectus proin
+          nibh nisl condimentum id venenatis. Tellus in hac habitasse platea
+          dictumst vestibulum rhoncus est. Dignissim cras tincidunt lobortis
+          feugiat. Vitae turpis massa sed elementum. Quis hendrerit dolor magna
+          eget est lorem ipsum dolor. Tortor vitae purus faucibus ornare
+          suspendisse sed nisi. Aliquam vestibulum morbi blandit cursus risus at
+          ultrices. Volutpat diam ut venenatis tellus in metus vulputate. Elit
+          sed vulputate mi sit amet mauris. Tortor consequat id porta nibh. Eu
+          tincidunt tortor aliquam nulla facilisi cras fermentum odio eu. Lacus
+          sed turpis tincidunt id aliquet risus feugiat. Sit amet risus nullam
+          eget felis. Viverra suspendisse potenti nullam ac tortor vitae purus
+          faucibus. Nibh mauris cursus mattis molestie a. In eu mi bibendum
+          neque egestas congue quisque. A arcu cursus vitae congue mauris
+          rhoncus aenean vel elit. Sit amet nulla facilisi morbi. Tempor nec
+          feugiat nisl pretium fusce id velit. Ac turpis egestas sed tempus. Sem
+          et tortor consequat id. Amet commodo nulla facilisi nullam vehicula
+          ipsum a arcu cursus. Est ante in nibh mauris. Vitae turpis massa sed
+          elementum tempus egestas. Ut consequat semper viverra nam libero justo
+          laoreet sit amet.
+        </p>
+        <br />
+        <br />
+        <h2>Lorem Ipsum</h2>
+        <br />
+        <br />
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Potenti
