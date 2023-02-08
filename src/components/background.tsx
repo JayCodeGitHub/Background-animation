768e6034a88751378205931221078b873f9cdc39
JayCodeGitHub <JayCode.contakt@gmail.com>
2023-02-08 03:23:56 +0100

    Added Parallax effect

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,17 +1,24 @@
+import { motion, useScroll, useTransform } from "framer-motion";
+
 export default function Background() {
+  let { scrollYProgress } = useScroll();
+  let y = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
   return (
-    <div className="absolute top-0 left-0 w-full h-screen bg-gray-900 -z-20">
+    <motion.div
+      style={{ y }}
+      className="absolute top-0 left-0 w-full h-full bg-gray-900 -z-20"
+    >
       <video
         muted
         playsInline
         autoPlay
         loop
-        className="object-cover object-center w-full h-full opacity-50 -z-10"
+        className="top-0 left-0 object-cover object-center w-full h-[99%] opacity-50 -z-10"
         src="https://www.w3schools.com/howto/rain.mp4"
       ></video>
-      <div className="absolute top-0 left-0 w-full h-screen -inset-px bg-gradient-to-t from-gray-900 to-transparent" />
+      <div className="absolute top-0 left-0 w-full h-full -inset-px bg-gradient-to-t from-gray-900 to-transparent" />
       <div className="h-full bg-gray-900" />
       <div className="h-full bg-gray-900" />
-    </div>
+    </motion.div>
   );
 }
