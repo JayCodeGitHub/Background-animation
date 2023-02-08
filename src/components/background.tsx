b73269ecee4708050043ff544c1447b0cb8a9e91
JayCodeGitHub <JayCode.contakt@gmail.com>
2023-02-08 03:13:13 +0100

    Added Simple Video as background

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,5 +1,17 @@
 export default function Background() {
   return (
-    <div className="absolute top-0 left-0 min-w-full min-h-screen bg-gray-500 -z-10"></div>
+    <div className="absolute top-0 left-0 w-full h-screen bg-gray-900 -z-20">
+      <video
+        muted
+        playsInline
+        autoPlay
+        loop
+        className="object-cover object-center w-full h-full opacity-50 -z-10"
+        src="https://www.w3schools.com/howto/rain.mp4"
+      ></video>
+      <div className="absolute top-0 left-0 w-full h-screen -inset-px bg-gradient-to-t from-gray-900 to-transparent" />
+      <div className="h-full bg-gray-900" />
+      <div className="h-full bg-gray-900" />
+    </div>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 export default function NavBar() {
   return (
-    <nav>
-      <h1>NavBar</h1>
+    <nav className="text-white">
+      <ul className="flex gap-8 p-8">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About Us</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
